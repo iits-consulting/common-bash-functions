f2c5705d3f1982d9e2f20df59d058c09d5bf79bd
Victor Getz <victor.getz@iits-consulting.de>
2025-10-27 14:35:19 +0100

    Update bash-functions.sh

--- bash-functions.sh
+++ bash-functions.sh
@@ -2,7 +2,7 @@
 
 function argo() {
 
-  local ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-secret -o jsonpath="{.data.admin\.password}" | base64 -d)
+  local ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
   echo "Username=admin, password=$ARGOCD_PASSWORD"
 
 
